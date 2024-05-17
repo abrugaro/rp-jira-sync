2437dd2a4f73c691c85d3932a7e9bf403fb84848
Alejandro Brugarolas <abrugaro@redhat.com>
2024-05-17 14:58:39 +0200

    task description improvement
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- src/adapters/task.adapter.ts
+++ src/adapters/task.adapter.ts
@@ -6,14 +6,19 @@ export function launchToTaskDescription(
   launch: ReportPortalLaunch,
   suiteItems: ReportPortalItem[]
 ): string {
+  const attrKeys = launch.attributes.map(attr => attr.key).join('||');
+  const attrValues = launch.attributes.map(attr => attr.value).join('|');
   const rpUrl = ENV.reportPortalApiUrl
     .replace("/api/", "/ui/")
     .concat(
       `/#${ENV.reportPortalProject}/launches/all/${launch.id}/${suiteItems[0].parent}`
     );
 
-  let description = `*RP Run #${launch.number}*\n *Jenkins:* ${launch.description}
-        \n\n*RP Suite errors URL:* ${rpUrl}\n\n----\n\n
+  let description = `*RP Suite errors URL (Please, analyze the failures in RP using the following link):* ${rpUrl}\n\n----\n\n
+        *RP Run #${launch.number}*\n *Jenkins:* ${launch.description}\n\n
+        *Launch Attributes*\n
+        ||${attrKeys}|\n
+        |${attrValues}|
     `;
   description += suiteItems.map((item) => item.description).join("\n");
 
